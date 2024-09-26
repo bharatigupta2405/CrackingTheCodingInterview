e249b8f9e1bcdb560cd4ecc5011909da6d04822e
Bharati Gupta <bharatigupta2405@gmail.com>
2024-09-26 21:03:09 +0530

    Update Q1.1_is_Unique.js

--- Arrays and Strings/Q1.1_is_Unique.js
+++ Arrays and Strings/Q1.1_is_Unique.js
@@ -1,1 +1,19 @@
 
+let S="abbsbaavshhsuon";
+let m=S.split("");
+let map={};
+for (let string in m){
+    if(!map[m[string]]){
+        map[m[string]]=1
+    }else{
+        map[m[string]]+=1
+    }
+}
+for( let i=0;i<m.length;i++){
+
+    if(map[m[i]]==1){
+        
+        console.log(m[i]+" is unique element");
+    }
+}
+// Time Complexity=O(n); Space complexity= O(n)
